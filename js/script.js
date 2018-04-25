6550f38b9e246bb1b7578bab19e418b1ada60095
Nicola Centrone <nicola.centrone.it@gmail.com>
2018-04-25 16:59:42 +0200

    style: Add comments to improve readability

--- js/script.js
+++ js/script.js
@@ -10,10 +10,9 @@ let time;
 let stars;
 let newTime;
 
-/* cardsFlipped: the cards that are flipped in this moment. They can be
+/* The cards that are flipped in this moment. They can be
 0, 1, 2. This is not the number of total cards flipped, neither the number of
-matches. */
-
+matches */
 let cardsFlipped = 0;
 
 let numberOfMatches = 0;
@@ -21,73 +20,226 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let countMoves = 0;
-let isCicleStarted = false;
+let isCycleStarted = false;
+
+/* To allow the refresh of timestamp only in the game screen and not in the
+ winning screen */
 let isPageCleared = false;
 let isGameRestarted = false;
 let isFirstClick = true;
 
-function setEventListeners () {
-  gameFlipper = document.querySelectorAll('.game__flipper');
-  for (let i = 0; i < gameFlipper.length; i++) {
-    let node = gameFlipper[i];
-    node.addEventListener('click', flipperLogic, false);
+/* If set to true, by clicking the main title you can win the game */
+const isDebugMode = true;
+
+/*
+* startGame is a container function for grouping all the functions to start
+* a new game
+*/
+function startGame () {
+  countMoves = 0;
+  isFirstClick = true;
+  createGameStruct();
+  shuffle(numberSeries);
+  setSeries(nodes, numberSeries);
+  setPictures();
+  setEventListeners();
+}
+
+/*
+* createGameStruct is a container function for grouping all the functions to
+* create a new game structure
+*/
+function createGameStruct () {
+  clearScreen();
+  createHeader();
+  createGameDiv();
+  createFlipperContainerDiv();
+  createFlipperDiv();
+  createBoxes();
+  isPageCleared = false;
+}
+
+/*
+* clearScreen deletes the topContainer div and repaints the page with a new empty
+* div
+* Called by: createGameStruct
+*/
+function clearScreen () {
+  let topContainer;
+  let newContainer;
+  let newHeader;
+  let newMain;
+  topContainer = document.querySelector('.top-container')
+  topContainer.remove();
+  newContainer = document.createElement('div');
+  newContainer.setAttribute('class', 'top-container');
+  document.querySelector('body').appendChild(newContainer);
+  newHeader = document.createElement('header');
+  newMain = document.createElement('main');
+  newContainer.appendChild(newHeader);
+  newContainer.appendChild(newMain);
+
+  // Every time the page is cleared the time is reset
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  isPageCleared = true;
+  return newContainer;
+}
+
+/*
+* createHeader groups various functions to build the top part of the game
+* Called by: createGameStruct
+*/
+function createHeader () {
+  let header;
+  let newDiv;
+  let newHeader;
+  let headerContainer;
+  let classes = ['header__container', 'stats__stars-container',
+  'stats__time-container', 'stats__move-container', 'stats__replay-container'];
+  header = document.querySelector('header');
+  for (let i = 0; i < 5; i++) {
+    newDiv = document.createElement('div');
+    newDiv.setAttribute('class', '' + classes[i]);
+    header.appendChild(newDiv);
+  }
+  newHeader = document.createElement('h1');
+
+  // For debug purpose only
+  if(isDebugMode == true) {
+    newHeader.addEventListener('click', winningScreen, false);
+  }
+
+  newHeader.innerHTML='Memory Game';
+  headerContainer = document.querySelector('.header__container');
+  headerContainer.appendChild(newHeader);
+  createTime();
+  createStars();
+  createMoves();
+  createRepeat();
+}
+
+/*
+* Prints the 00:00:00 timestamp in the header
+* Called by: createHeader()
+*/
+function createTime (){
+  let newHdr;
+  newHdr = document.createElement('h3');
+  document.querySelector('.stats__time-container').appendChild(newHdr);
+  newTime = document.createElement('time');
+  newTime.innerHTML = '00:00:00';
+  newTime.setAttribute('class', 'time');
+  newHdr.appendChild(newTime);
+}
+
+/*
+* Prints the three stars in the header
+* Called by: createHeader()
+*/
+function createStars () {
+  let starsContainer;
+  let newP;
+  starsContainer = document.querySelector('.stats__stars-container');
+  newP = document.createElement('p');
+  newP.setAttribute('class', 'stars');
+  newP.innerHTML = '★ ★ ★';
+  starsContainer.appendChild(newP);
+}
+
+/*
+* Prints the number of moves
+* Called by: createHeader()
+*/
+function createMoves () {
+  let moveContainer;
+  let newHdr;
+  moveContainer = document.querySelector('.stats__move-container');
+  newHdr = document.createElement('h3');
+  newHdr.innerHTML = 'Moves: ' +countMoves;
+  moveContainer.appendChild(newHdr);
+}
+
+/*
+* Prints the in-game repeat icon in the header and starts the eventListener
+* Called by: createHeader()
+*/
+function createRepeat () {
+  let replayContainer;
+  let newDiv;
+  replayContainer = document.querySelector('.stats__replay-container');
+  newDiv = document.createElement('div');
+  replayContainer.appendChild(newDiv);
+  newDiv.setAttribute('class', 'repeat-button__container');
+  newDiv.style.backgroundImage = 'url(img/repeat.png)';
+  newDiv.style.backgroundSize = '28px 28px';
+  newDiv.addEventListener('click', reStartGame, false);
+}
+
+/*
+* Creates the main game div container
+* Called by: createGameStruct
+*/
+function createGameDiv () {
+  const MAIN = document.querySelector('main');
+  const NEW_GAME_DIV = document.createElement('div');
+  MAIN.appendChild(NEW_GAME_DIV);
+  NEW_GAME_DIV.setAttribute('class', 'game');
+}
+
+/*
+* Element needed as background for rotation
+* Called by: createGameStruct()
+*/
+function createFlipperContainerDiv () {
+  let gameDiv;
+  let newFlipperContainerDiv;
+  for (let i = 0; i < 16; i++) {
+    gameDiv = document.querySelector('.game');
+    newFlipperContainerDiv = document.createElement('div');
+    gameDiv.appendChild(newFlipperContainerDiv);
+    newFlipperContainerDiv.setAttribute('class', 'game__flipper-container');
   }
 }
 
-function flipperLogic (event) {
-  if (isFirstClick == true) {
-    timer();
-  }
-  isFirstClick = false;
-  if (numberOfMatches < 8) {
-    if ((cardsFlipped == 0 && isCicleStarted == false) && (event.target.classList[0] == 'game__box-front')) {
-      isCicleStarted = true;
-      flip(event);
-      cardsFlipped ++;
-      number1 = event.target.nextElementSibling.classList[1];
-      eventTarget1 = event.target;
-    } else if ((cardsFlipped == 1 && isCicleStarted == true) && (event.target.classList[0] == 'game__box-front')) {
-      flip(event);
-      cardsFlipped ++;
-      number2 = event.target.nextElementSibling.classList[1];
-      eventTarget2 = event.target;
-      if (number1 != number2) {
-        countMoves ++;
-        unflip(eventTarget1, eventTarget2);
-      } else {
-        countMoves ++;
-        cardsFlipped = 0;
-        isCicleStarted = false;
-        numberOfMatches ++;
-        if (numberOfMatches == 8) {
-          winningScreen();
-        }
-      }
-    }
-  }
-  if (numberOfMatches < 8) {
-    starsLogic();
-    clearMoves();
-    createMoves();
-  } else {
-    numberOfMatches = 0;
+/*
+* Element that actually flips
+Called by: createGameStruct()
+*/
+function createFlipperDiv () {
+  let newFlipperDiv;
+  gameFlipperContainers = document.querySelectorAll('.game__flipper-container');
+  for (let i = 0; i < gameFlipperContainers.length; i++) {
+    newFlipperDiv = document.createElement('div');
+    newFlipperDiv.setAttribute('class', 'game__flipper')
+    gameFlipperContainers[i].appendChild(newFlipperDiv);
   }
 }
 
-function flip (evt) {
-  evt.target.parentNode.setAttribute('class', 'game__flipper flip');
+/*
+* Create the elements that will show the background image and the blue color
+* Called by: createGameStruct()
+*/
+function createBoxes () {
+  let newBoxFront;
+  let newBoxBack;
+  gameFlipperContainers = document.querySelectorAll('.game__flipper');
+  for (let i = 0; i < gameFlipperContainers.length; i++) {
+    newBoxFront = document.createElement('div');
+    newBoxFront.setAttribute('class', 'game__box-front')
+    newBoxBack = document.createElement('div');
+    newBoxBack.setAttribute('class', 'game__box-back')
+    gameFlipperContainers[i].appendChild(newBoxFront);
+    gameFlipperContainers[i].appendChild(newBoxBack);
+  }
 }
 
-function unflip (node1, node2) {
-  setTimeout(function() {
-    node1.parentNode.setAttribute('class', 'game__flipper');
-    node2.parentNode.setAttribute('class', 'game__flipper');
-    cardsFlipped = 0;
-    isCicleStarted = false;
-  }, 1000);
-
-}
-
+/*
+* Takes a string of 16 number as parameter and shuffle them
+* Called by: startGame()
+*/
 function shuffle(a) {
     let j, x, i;
     for (i = a.length - 1; i > 0; i--) {
@@ -98,6 +250,11 @@ function shuffle(a) {
     }
 }
 
+/*
+* Takes the shuffled numbers and arrange them as a new class for the existing
+* class .game__box-back
+* Called by: startGame()
+*/
 function setSeries (nodes, numberSeries) {
   let node;
   nodes = [];
@@ -113,6 +270,11 @@ function setSeries (nodes, numberSeries) {
   }
 }
 
+/*
+* Set a picture to every number of numberSeries. In every game each number has
+* always the same picture but this is invisible to the player.
+* Called by: startGame()
+*/
 function setPictures () {
   let num;
   for (let i = 0; i < gameBoxBack.length; i++) {
@@ -143,6 +305,93 @@ function setPictures () {
       gameBoxBack[i].setAttribute('alt', 'Squirtle');
     }
   }
+}
+
+/*
+* Every div with class .game__flipper act as a container listening for a click
+* Called by: startGame()
+*/
+function setEventListeners () {
+  gameFlipper = document.querySelectorAll('.game__flipper');
+  for (let i = 0; i < gameFlipper.length; i++) {
+    let node = gameFlipper[i];
+    node.addEventListener('click', flipperLogic, false);
+  }
+}
+
+/*
+* Main game logic
+* Called: when the player clicks a cell
+*/
+function flipperLogic (event) {
+  // Needed to check if it is the first click, because it starts the timer once
+  if (isFirstClick == true) {
+    timer();
+  }
+  isFirstClick = false;
+
+  // To win, a player needs to get 8 matches
+  if (numberOfMatches < 8) {
+
+    /* Is the first card to be clicked? We need also to prevent an error when a
+    player keeps clicking constantly on a rotated card */
+    if ((cardsFlipped == 0 && isCycleStarted == false) && (event.target.classList[0] == 'game__box-front')) {
+      isCycleStarted = true;
+      flip(event);
+      cardsFlipped ++;
+      number1 = event.target.nextElementSibling.classList[1];
+      eventTarget1 = event.target;
+
+    /* Is a card is already turned? We need also to prevent an error when a
+    player keeps clicking constantly on a rotated card */
+    } else if ((cardsFlipped == 1 && isCycleStarted == true) && (event.target.classList[0] == 'game__box-front')) {
+      flip(event);
+      cardsFlipped ++;
+      number2 = event.target.nextElementSibling.classList[1];
+      eventTarget2 = event.target;
+
+      // Match or not?
+      if (number1 != number2) {
+        countMoves ++;
+        unflip(eventTarget1, eventTarget2);
+      } else {
+        countMoves ++;
+        cardsFlipped = 0;
+        isCycleStarted = false;
+        numberOfMatches ++;
+        if (numberOfMatches == 8) {
+          winningScreen();
+        }
+      }
+    }
+  }
+  if (numberOfMatches < 8) {
+    starsLogic();
+    clearMoves();
+    createMoves();
+  } else {
+    numberOfMatches = 0;
+  }
+}
+
+/*
+* flip is used to apply the class flip that enables the CSS rotation
+*/
+function flip (evt) {
+  evt.target.parentNode.setAttribute('class', 'game__flipper flip');
+}
+
+/*
+* unflip is used to delete the class flip that enables the CSS rotation
+*/
+function unflip (node1, node2) {
+  setTimeout(function() {
+    node1.parentNode.setAttribute('class', 'game__flipper');
+    node2.parentNode.setAttribute('class', 'game__flipper');
+    cardsFlipped = 0;
+    isCycleStarted = false;
+  }, 1000);
+
 }
 
 function winningScreen () {
@@ -208,129 +457,16 @@ function createGameHeaderDiv () {
   NEW_GAME_DIV.setAttribute('class', 'header');
 }
 
-function createGameDiv () {
-  const MAIN = document.querySelector('main');
-  const NEW_GAME_DIV = document.createElement('div');
-  MAIN.appendChild(NEW_GAME_DIV);
-  NEW_GAME_DIV.setAttribute('class', 'game');
-}
-
-function createFlipperContainerDiv () {
-  let gameDiv;
-  let newFlipperContainerDiv;
-  for (let i = 0; i < 16; i++) {
-    gameDiv = document.querySelector('.game')
-    newFlipperContainerDiv = document.createElement('div');
-    gameDiv.appendChild(newFlipperContainerDiv);
-    newFlipperContainerDiv.setAttribute('class', 'game__flipper-container');
-  }
-}
-
-function createFlipperDiv () {
-  let newFlipperDiv;
-  gameFlipperContainers = document.querySelectorAll('.game__flipper-container');
-  for (let i = 0; i < gameFlipperContainers.length; i++) {
-    newFlipperDiv = document.createElement('div');
-    newFlipperDiv.setAttribute('class', 'game__flipper')
-    gameFlipperContainers[i].appendChild(newFlipperDiv);
-  }
-}
-
-function createBoxes () {
-  let newBoxFront;
-  let newBoxBack;
-  gameFlipperContainers = document.querySelectorAll('.game__flipper');
-  for (let i = 0; i < gameFlipperContainers.length; i++) {
-    newBoxFront = document.createElement('div');
-    newBoxFront.setAttribute('class', 'game__box-front')
-    newBoxBack = document.createElement('div');
-    newBoxBack.setAttribute('class', 'game__box-back')
-    gameFlipperContainers[i].appendChild(newBoxFront);
-    gameFlipperContainers[i].appendChild(newBoxBack);
-  }
-}
-
-function clearScreen () {
-  let topContainer;
-  let newContainer;
-  let newHeader;
-  let newMain;
-  topContainer = document.querySelector('.top-container')
-  topContainer.remove();
-  newContainer = document.createElement('div');
-  newContainer.setAttribute('class', 'top-container');
-  document.querySelector('body').appendChild(newContainer);
-  newHeader = document.createElement('header');
-  newMain = document.createElement('main');
-  newContainer.appendChild(newHeader);
-  newContainer.appendChild(newMain);
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  isPageCleared = true;
-  return newContainer;
-}
-
-function createGameStruct () {
-  clearScreen();
-  createHeader();
-  createGameDiv();
-  createFlipperContainerDiv();
-  createFlipperDiv();
-  createBoxes();
-  isPageCleared = false;
-}
-
-function startGame () {
-  countMoves = 0;
-  isFirstClick = true;
-  createGameStruct();
-  shuffle(numberSeries);
-  setSeries(nodes, numberSeries);
-  setPictures();
-  setEventListeners();
-}
-
-function createHeader () {
-  let header;
-  let newDiv;
-  let newHeader;
-  let headerContainer;
-  let classes = ['header__container', 'stats__stars-container',
-  'stats__time-container', 'stats__move-container', 'stats__replay-container'];
-
-  header = document.querySelector('header');
-  for (let i = 0; i < 5; i++) {
-    newDiv = document.createElement('div');
-    newDiv.setAttribute('class', '' + classes[i]);
-    header.appendChild(newDiv);
-  }
-  newHeader = document.createElement('h1');
-
-  newHeader.addEventListener('click', winningScreen, false);
-
-  newHeader.innerHTML='Memory Game';
-  headerContainer = document.querySelector('.header__container');
-  headerContainer.appendChild(newHeader);
-  createTime();
-  createStars();
-  createMoves();
-  createRepeat();
-}
-
-function createTime (){
-  newHdr = document.createElement('h3');
-  document.querySelector('.stats__time-container').appendChild(newHdr);
-  newTime = document.createElement('time');
-  newTime.innerHTML = '00:00:00';
-  newTime.setAttribute('class', 'time');
-  newHdr.appendChild(newTime);
-}
-
+/*
+* Used setTimeout as time counter
+*/
 function timer () {
   time = setTimeout(addTime, 1000);
 }
 
+/*
+* addTime updates the timestamp and keeps cycling timer()
+*/
 function addTime() {
     seconds ++;
     if (seconds >= 60) {
@@ -348,16 +484,9 @@ function addTime() {
     }
 }
 
-function createStars () {
-  let starsContainer;
-  let newP;
-  starsContainer = document.querySelector('.stats__stars-container');
-  newP = document.createElement('p');
-  newP.setAttribute('class', 'stars');
-  newP.innerHTML = '★ ★ ★';
-  starsContainer.appendChild(newP);
-}
-
+/*
+* Called by: starsLogic()
+*/
 function starsLogic () {
   stars = document.querySelector('.stars');
   if(countMoves >10 && countMoves <= 15) {
@@ -369,33 +498,19 @@ function starsLogic () {
   }
 }
 
-function createMoves () {
-  let moveContainer;
-  let newHdr;
-  moveContainer = document.querySelector('.stats__move-container');
-  newHdr = document.createElement('h3');
-  newHdr.innerHTML = 'Moves: ' +countMoves;
-  moveContainer.appendChild(newHdr);
-}
-
+/*
+* Called by: flipperLogic()
+*/
 function clearMoves () {
   let moveContainer;
   moveContainer = document.querySelector('.stats__move-container');
   moveContainer.firstChild.remove();
 }
 
-function createRepeat () {
-  let replayContainer;
-  let newDiv;
-  replayContainer = document.querySelector('.stats__replay-container');
-  newDiv = document.createElement('div');
-  replayContainer.appendChild(newDiv);
-  newDiv.setAttribute('class', 'repeat-button__container');
-  newDiv.style.backgroundImage = 'url(img/repeat.png)';
-  newDiv.style.backgroundSize = '28px 28px';
-  newDiv.addEventListener('click', reStartGame, false);
-}
-
+/*
+* Called by: createRepeat() this is triggered when the user clicks the in-game
+* restart button
+*/
 function reStartGame () {
   isGameRestarted = true;
   clearTimeout(time);
